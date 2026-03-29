67ee2782f902ebc4d83eba4721e4ce2b32a64548
Nityom <nityomtikherr@gmail.com>
2026-03-29 13:11:07 +0530

    feat: wrap ResetPassword component in Suspense for improved loading handling

--- app/auth/reset_password/page.tsx
+++ app/auth/reset_password/page.tsx
@@ -1,6 +1,11 @@
 // app/reset-password/page.tsx
+import { Suspense } from "react";
 import ResetPassword from "@/components/ResetPassword";
 
 export default function ResetPasswordPage() {
-  return <ResetPassword />;
+  return (
+    <Suspense fallback={null}>
+      <ResetPassword />
+    </Suspense>
+  );
 }
